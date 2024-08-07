d919f091fac4749db4735f6765aba97ae87b0caf
ShenganLee <lsa1669778762@gmail.com>
2024-08-07 10:54:44 +0800

    example

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as SaExpoWebserver from '@sa/expo-webserver';
+import { WebServer } from "@sa/expo-webserver";
+import { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [uri, setUri] = useState("");
+
+  useEffect(() => {
+    const server = new WebServer();
+
+    server
+      .start("", [{ Path: "/", Target: "https://baidu.com" }])
+      .then((uri) => {
+        setUri(uri);
+      });
+
+    return () => {
+      server.destory();
+    };
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>{SaExpoWebserver.hello()}</Text>
-    </View>
+    <View style={styles.container}>{uri && <WebView source={{ uri }} />}</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
